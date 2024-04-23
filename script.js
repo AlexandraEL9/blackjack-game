c8c7babd060e48e1745d57d91af232a098de1f3a
AlexandraEL9 Lee <alexandraelee9@gmail.com>
2024-04-23 10:25:58 +0000

    feat:tinkered with button

--- script.js
+++ script.js
@@ -1,22 +1,25 @@
-let firstCard = 10
-let secondCard = 4
-let sum = firstCard + secondCard
-let hasBlackJack = false
-let isAlive = true
-let message = ""
+let firstCard = 10;
+        let secondCard = 4;
+        let sum = firstCard + secondCard;
+        let hasBlackJack = false;
+        let isAlive = true;
+        let message = "";
+        
+        // Create a function to update the message element
+        function setMessage(message) {
+            document.getElementById("message-el").textContent = message;
+        }
 
-// 2. Create a startGame() function. Move the conditional
-// below (line 11-20) inside the body of the function.
-function startGame() {
-    if (sum <= 20) {
-        message = "Do you want to draw a new card? 🙂"
-    } else if (sum === 21) {
-        message = "Wohoo! You've got Blackjack! 🥳"
-        hasBlackJack = true
-    } else {
-        message = "You're out of the game! 😭"
-        isAlive = false
-    }
-}
+        // Create a startGame() function
+        function startGame() {
+            if (sum <= 20) {
+                message = "Do you want to draw a new card? 🙂";
+            } else if (sum === 21) {
+                message = "Wohoo! You've got Blackjack! 🥳";
+                hasBlackJack = true;
+            } else {
+                message = "You're out of the game! 😭";
+                isAlive = false;
+            }
 
-console.log(message)
+            // Update the message element
